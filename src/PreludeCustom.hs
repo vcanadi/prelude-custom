144a0fe35d2ac99d1b5465b6d270df6366c4b1eb
Vito Canadi <vito.canadi@gmail.com>
2017-04-13 00:32:07 +0200

    mbool, mboolFunc functions added. Data.Function package included

--- src/PreludeCustom.hs
+++ src/PreludeCustom.hs
@@ -8,3 +8,11 @@ module PreludeCustom
 import Prelude         as ReEx
 import Data.Bool       as ReEx
 import Data.Monoid     as ReEx
+import Data.Function   as ReEx
+
+
+mbool :: (Monoid a) => a -> Bool -> a
+mbool = bool mempty
+
+mboolFunc :: (Monoid a) => (b -> a) -> Bool -> b -> a
+mboolFunc f b = flip mbool b . f
