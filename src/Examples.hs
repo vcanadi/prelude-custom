9a1a6e8c8beaf50a04f63388a74b3655eab96b4a
Vito Canadi <vito.canadi@gmail.com>
2017-04-18 15:32:10 +0200

    All generalized if-then-else functions implemented. Infix operators changed (??> to ?>>>)

--- src/Examples.hs
+++ src/Examples.hs
@@ -4,29 +4,29 @@ import PreludeCustom
 import Debug.Trace
 
 fac :: Int -> Int
-fac = (==0) ??> const 1
-            ||> (*) <$> id
-                    <*> fac . pred
+fac = (==0) ?>>> const 1
+             ||> (*) <$> id
+                     <*> fac . pred
 
 -- can't check execution of this but implementations looks neat
 collatz :: Int -> Int
-collatz = (==1) ??> id
-                ||> even ??> collatz . (`div` 2)
-                         ||> collatz . succ . (*3)
+collatz = (==1) ?>>> id
+                 ||> even ?>>> collatz . (`div` 2)
+                           ||> collatz . succ . (*3)
 
 
 -- implementation looks messy but result is list of input values
 -- collatz branch to (merged result of first rec call and input) and (merger result of second rec call input)
 collatzAcc :: Int -> [Int]
-collatzAcc = (==1) ??> return
-                   ||> even ??> (<>) <$> collatzAcc . (`div` 2)
-                                     <*> return
-                            ||> (<>) <$> collatzAcc . succ . (*3)
-                                     <*> return
+collatzAcc = (==1) ?>>> return
+                   ||> even ?>>> (<>) <$> collatzAcc . (`div` 2)
+                                      <*> return
+                             ||> (<>) <$> collatzAcc . succ . (*3)
+                                      <*> return
 
 -- merge (result of collatz branch) and input
 collatzAcc2 :: Int -> [Int]
-collatzAcc2 = (==1) ??> return
-                    ||> (<>) <$> ( even  ??>  collatzAcc2 . (`div` 2)
-                                         ||>  collatzAcc2 . succ . (*3) )
-                             <*> return
+collatzAcc2 = (==1) ?>>> return
+                     ||> (<>) <$> ( even  ?>>>  collatzAcc2 . (`div` 2)
+                                           ||>  collatzAcc2 . succ . (*3) )
+                              <*> return
--- src/PreludeCustom.hs
+++ src/PreludeCustom.hs
@@ -15,46 +15,71 @@ import Data.Bifunctor  as ReEx (bimap)
 import Control.Arrow   as ReEx
 
 
--- same as bool without value for false case
-monoidBool :: (Monoid a) => a -> Bool -> a
-monoidBool = bool mempty
 
-
--- if A then B else C where A,B,C <- [V (value), F (function), Z (zero, mempty, for monoid type)]
+-- if A then B [else C] where A,B,C e {V (value), F (function)}  (optional C is for monoid type where else is mempty)
 -- example if Value then Function else Function
 type V a   = a
 type F a b = (a -> b)
 
+type IfVVV b   = V Bool   ->  V b    ->  V b    ->  V b
+type IfVVF a b = V Bool   ->  V b    ->  F a b  ->  F a b
+type IfVFV a b = V Bool   ->  F a b  ->  V b    ->  F a b
 type IfVFF a b = V Bool   ->  F a b  ->  F a b  ->  F a b
+type IfFVV a b = F a Bool ->  V b    ->  V b    ->  F a b
+type IfFVF a b = F a Bool ->  V b    ->  F a b  ->  F a b
+type IfFFV a b = F a Bool ->  F a b  ->  V b    ->  F a b
 type IfFFF a b = F a Bool ->  F a b  ->  F a b  ->  F a b
 
-type IfVVZ a   = V Bool   ->  V a    ->  V a
-type IfVFZ a b = V Bool   ->  F a b  ->  F a b
-type IfFFZ a b = F a Bool ->  F a b  ->  F a b
+
+ifVFF, (?->>)  ::             IfVFF a b
+ifFFF, (?>>>)  ::             IfFFF a b
+
+
+ifVVV b x y = bool y x b
+ifVVF b x g = bool <$> g       <*> const x <*> const b
+ifVFV b f y = bool <$> const y <*> f       <*> const b
+ifVFF b f g = bool <$> g       <*> f       <*> const b
+ifFVV p x y = bool <$> const y <*> const x <*> p
+ifFVF p x g = bool <$> g       <*> const x <*> p
+ifFFV p f y = bool <$> const y <*> f       <*> p
+ifFFF p f g = bool <$> g       <*> f       <*> p
+
+ifVV b x = ifVVV b x mempty
+ifVF b f = ifVFV b f mempty
+ifFV p x = ifFVV p x mempty
+ifFF p f = ifFFV p f mempty
 
 
 
-ifVFF        ::             IfVFF a b
-ifFFF, (??>) ::             IfFFF a b
-
-ifVVZ        :: Monoid a => IfVVZ a
-ifVFZ        :: Monoid b => IfVFZ a b
-ifFFZ, (?>)  :: Monoid b => IfFFZ a b
-
-
-ifVFF b f g = bool <$> g <*> f <*> const b
-ifFFF p f g = bool <$> g <*> f <*> p
-
-ifVVZ b x   = monoidBool x b
-ifVFZ b f   = flip monoidBool b . f
-ifFFZ p f   = monoidBool <$> f <*> p
+infixr 2 ?---, ?-->, ?->-, ?->>, ?>--, ?>->, ?>>-, ?>>>
+(?---) = ifVVV
+(?-->) = ifVVF
+(?->-) = ifVFV
+(?->>) = ifVFF
+(?>--) = ifFVV
+(?>->) = ifFVF
+(?>>-) = ifFFV
+(?>>>) = ifFFF
 
 
-infixr 2 ?>
-(?>) = ifFFZ
 
-infixr 2 ??>
-(??>)       = ifFFF
+type IfVV  b   = V Bool   ->  V b    ->  V b
+type IfVF  a b = V Bool   ->  F a b  ->  F a b
+type IfFV  a b = F a Bool ->  V b    ->  F a b
+type IfFF  a b = F a Bool ->  F a b  ->  F a b
+
+
+ifVV,  (?--)   :: Monoid a => IfVV a
+ifVF,  (?->)   :: Monoid b => IfVF a b
+ifFV,  (?>-)   :: Monoid b => IfFV a b
+ifFF,  (?>>)   :: Monoid b => IfFF a b
+
+
+infixr 2 ?--, ?->, ?>-, ?>>
+(?--) = ifVV
+(?->) = ifVF
+(?>-) = ifFV
+(?>>) = ifFF
 
 infixr 1 ||>
 (||>)       = ($)
