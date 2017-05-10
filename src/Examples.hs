f9f26265d754da30c8b9e9dd1c7a749048985ba0
Vito Canadi <vito.canadi@gmail.com>
2017-05-10 20:24:27 +0200

    minor change in examples

--- src/Examples.hs
+++ src/Examples.hs
@@ -4,13 +4,13 @@ import PreludeCustom
 import Debug.Trace
 
 fac :: Int -> Int
-fac = (==0) ?>>> const 1
+fac = (==0) ?>-> 1
              ||> (*) <$> id
                      <*> fac . pred
 
 -- can't check execution of this but implementations looks neat
 collatz :: Int -> Int
-collatz = (==1) ?>>> id
+collatz = (==1) ?>-> 1
                  ||> even ?>>> collatz . (`div` 2)
                            ||> collatz . succ . (*3)
 
@@ -18,15 +18,15 @@ collatz = (==1) ?>>> id
 -- implementation looks messy but result is list of input values
 -- collatz branch to (merged result of first rec call and input) and (merger result of second rec call input)
 collatzAcc :: Int -> [Int]
-collatzAcc = (==1) ?>>> return
-                   ||> even ?>>> (<>) <$> collatzAcc . (`div` 2)
-                                      <*> return
-                             ||> (<>) <$> collatzAcc . succ . (*3)
-                                      <*> return
+collatzAcc = (==1) ?>-> [1]
+                    ||> even ?>>> (<>) <$> collatzAcc . (`div` 2)
+                                       <*> return
+                              ||> (<>) <$> collatzAcc . succ . (*3)
+                                       <*> return
 
 -- merge (result of collatz branch) and input
 collatzAcc2 :: Int -> [Int]
-collatzAcc2 = (==1) ?>>> return
+collatzAcc2 = (==1) ?>-> [1]
                      ||> (<>) <$> ( even  ?>>>  collatzAcc2 . (`div` 2)
                                            ||>  collatzAcc2 . succ . (*3) )
                               <*> return
